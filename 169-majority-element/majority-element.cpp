35df128fe42ebcc77e240e01ecf26dc067579bdc
harshita20-10 <harshitapandey274@gmail.com>
2026-05-19 06:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.07%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-     map<int,int>mpp;
+        
+        int candidate = 0;
+        int count = 0;
+
+        for(int num : nums) {
+
+            if(count == 0) {
+                candidate = num;
+            }
+
+            if(num == candidate) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+};
+        
+    /* map<int,int>mpp;
      for(int i=0;i<nums.size();i++){
         mpp[nums[i]]++;
      }
@@ -14,3 +36,4 @@ public:
     }
     
 };
+*/
